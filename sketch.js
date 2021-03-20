663bee77377a50ab0b5d68c502658de395a13eef
yslc8226 <75553336+yslc8226@users.noreply.github.com>
2021-03-20 10:59:03 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var gameState=0;
+var playerCount;
+var form, game, player;
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+  game=new Game();
+  game.getState();
+  game.start();
+
+  
+}
+
+function draw(){
+  background("white");
+ 
+}
+
+
